928f5ac986ad035d716133b34acc4b212a8ade80
javier marquez <hello.jmarquez@gmail.com>
2023-05-19 08:24:51 -0500

    verify mail and phone exists in register added

--- api/src/controllers/userRegister.js
+++ api/src/controllers/userRegister.js
@@ -1,8 +1,27 @@
 const { hash } = require('bcrypt')
 const { User } = require('../db.js')
+const data = {
+	statusCode: 200,
+	errors: {},
+}
 const userRegister = async (req, res) => {
 	const { password, name, email, phone } = req.body
 	const hashedPassword = await hash(password, 12)
+	const emailExist = User.findOne({
+		where: { email },
+	}).then(user => user)
+	const phoneExist = User.findOne({
+		where: { phone },
+	}).then(user => user)
+	if (emailExist) {
+		data.statusCode = 401
+		data.errors.email = 'Este email ya está registrado'
+	}
+	if (phoneExist) {
+		data.statusCode = 401
+		data.errors.email = 'Este teléfono ya está registrado'
+	}
+	if (data.statusCode !== 200) return res.send(data)
 	User.create({
 		name,
 		email,
@@ -10,7 +29,7 @@ const userRegister = async (req, res) => {
 		password: hashedPassword,
 	})
 		.then(nuevoUsuario => {
-			res.status(200).json(nuevoUsuario.toJSON())
+			res.status(200).send()
 		})
 		.catch(error => {
 			res.status(401).send(error)
