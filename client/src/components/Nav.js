5b9d796899b2173dc069c7ef780064c390a76985
javier marquez <hello.jmarquez@gmail.com>
2023-05-27 11:56:13 -0500

    node modules added to prettierignore

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from '../screens/Home';
-import Profile from '../screens/Profile';
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from '../screens/Home'
+import Profile from '../screens/Profile'
 
-
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const Nav = () => {
-    return (
-        <Stack.Navigator screenOptions={{headerShown: false}} >
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Home" component={Home} />
-       
-      </Stack.Navigator>
-    );
+	return (
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen name='Profile' component={Profile} />
+			<Stack.Screen name='Home' component={Home} />
+		</Stack.Navigator>
+	)
 }
 
 const styles = StyleSheet.create({})
 
-export default Nav;
+export default Nav
