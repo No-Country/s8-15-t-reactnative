7be48f474bb63d995e2acf138e596ae9d133821c
javier marquez <hello.jmarquez@gmail.com>
2023-05-29 09:08:42 -0500

    background image added

--- client/src/components/CambiarDinero/CambiarDinero.jsx
+++ client/src/components/CambiarDinero/CambiarDinero.jsx
@@ -1,41 +1,47 @@
-import { Text, View } from 'react-native'
+import { Text, View, ImageBackground } from 'react-native'
 import colors from '../../utils/colors'
 import fondo from '../../Images/fondo.webp'
 import { MaterialIcons } from '@expo/vector-icons'
 const CambiarDinero = () => {
 	return (
 		<View className='flex items-center '>
-			<View className='flex bg-blue-400 rounded-b-[40px] pt-6 h-56 w-full items-center'>
-				<View className='flex flex-row w-full justify-around items-center mb-3 px-4'>
-					<MaterialIcons name='notifications-none' size={27} color='white' />
-					<Text className='text-slate-50 font-black text-lg'>
-						Cryptomonedas
-					</Text>
-					<View className='w-10 h-10 bg-slate-100 rounded-full'></View>
-				</View>
-				<Text className='text-slate-50 mb-3 '>Portfolio Total</Text>
-				<Text className='text-slate-50 font-black text-3xl'>US $3.214</Text>
-				<View className='flex flex-row justify-around items-center mt-7 w-11/12  '>
-					<View className='bg-slate-50 p-2 rounded-t-2xl w-10/12 px-4'>
-						<Text>Cambiar</Text>
-						<View className='flex flex-row justify-between items-center'>
-							<View className='flex flex-row items-center'>
-								<MaterialIcons name='money-off' size={30} color='#6bbafb' />
-								<Text>Bitcoin</Text>
+			<ImageBackground
+				source={require('../../../assets/fondoHistorial.webp')}
+				resizeMode='cover'
+			>
+				<View className='flex rounded-b-[40px] pt-6 h-56 w-full items-center'>
+					<View className='flex flex-row w-full justify-around items-center mb-3 px-4'>
+						<MaterialIcons name='notifications-none' size={27} color='white' />
+						<Text className='text-slate-50 font-black text-lg'>
+							Cryptomonedas
+						</Text>
+						<View className='w-10 h-10 bg-slate-100 rounded-full'></View>
+					</View>
+					<Text className='text-slate-50 mb-3 '>Portfolio Total</Text>
+					<Text className='text-slate-50 font-black text-3xl'>US $3.214</Text>
+					<View className='flex flex-row justify-around items-center mt-7 w-11/12  '>
+						<View className='bg-slate-50 p-2 rounded-t-2xl w-10/12 px-4'>
+							<Text>Cambiar</Text>
+							<View className='flex flex-row justify-between items-center'>
+								<View className='flex flex-row items-center'>
+									<MaterialIcons name='money-off' size={30} color='#6bbafb' />
+									<Text>Bitcoin</Text>
+								</View>
+								<View>
+									<Text className='font-black text-blue-600 text-lg'>4000</Text>
+								</View>
 							</View>
-							<View>
-								<Text className='font-black text-blue-600 text-lg'>4000</Text>
-							</View>
-						</View>
-						<View className='flex flex-row justify-between '>
-							<Text className='text-slate-400 text-[12px]'>000425 BTC</Text>
-							<View>
-								<Text className=' text-blue-600 text-[12px]'>BTC</Text>
+							<View className='flex flex-row justify-between '>
+								<Text className='text-slate-400 text-[12px]'>000425 BTC</Text>
+								<View>
+									<Text className=' text-blue-600 text-[12px]'>BTC</Text>
+								</View>
 							</View>
 						</View>
 					</View>
 				</View>
-			</View>
+			</ImageBackground>
+
 			<Text className='font-black mt-20 text-violet-600 text-3xl'>0,40000</Text>
 			<View className='flex flex-row justify-between w-10/12  gap-2'>
 				<View className='flex w-14'>
