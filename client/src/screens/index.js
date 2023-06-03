f9bbeb0208918fb43e14d14067470dab7a5c411d
javier marquez <hello.jmarquez@gmail.com>
2023-06-03 08:03:11 -0500

    header arreglado

--- client/src/screens/index.js
+++ client/src/screens/index.js
@@ -1,11 +1,11 @@
-export * from './CryptoList';
-export * from './CryptoMetric';
-export * from './CryptoTransferencia';
-export * from './Historial';
-export * from './Security/Index';
-export { default as Home } from './Home';
-export * from './Onboarding/Index';
-export { default as Onboarding } from './Onboarding/Onboarding';
-export { default as Profile } from './Profile';
-export { default as ScannQr } from './ScannQr';
-export { default as Security } from './Security/Security';
+export * from './CryptoList'
+export * from './CryptoMetric'
+export * from './CryptoTransferencia'
+export * from './Historial'
+export * from './Security/Index'
+export { default as Home } from './Home'
+export * from './Onboarding/Index'
+export { default as Onboarding } from './Onboarding/Onboarding'
+export { default as Profile } from './Profile'
+export { default as ScannQr } from './ScannQr'
+export { default as Security } from './Security/Security'
