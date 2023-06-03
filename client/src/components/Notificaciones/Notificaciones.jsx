98a49f12c0557d032bfb199073c722ad0462b7e8
javier marquez <hello.jmarquez@gmail.com>
2023-06-03 10:16:18 -0500

    switches añadidos

--- client/src/components/Notificaciones/Notificaciones.jsx
+++ client/src/components/Notificaciones/Notificaciones.jsx
@@ -3,11 +3,19 @@ import colors from '../../utils/colors'
 import { MaterialIcons } from '@expo/vector-icons'
 import { useState } from 'react'
 const Notificaciones = () => {
-	const [isEnabled, setIsEnabled] = useState(false)
-	const toggleSwitch = () => setIsEnabled(previousState => !previousState)
+	const [isAll, setIsAll] = useState(false)
+	const toggleAll = () => setIsAll(previousState => !previousState)
+	const [istransfer, setIsTransfer] = useState(false)
+	const toggleTransfer = () => setIsTransfer(previousState => !previousState)
+	const [isIngresos, setIsIngresos] = useState(false)
+	const toggleIngresos = () => setIsIngresos(previousState => !previousState)
+	const [isPagos, setIsPagos] = useState(false)
+	const togglePagos = () => setIsPagos(previousState => !previousState)
+	const [isLinks, setIsLinks] = useState(false)
+	const toggleLinks = () => setIsLinks(previousState => !previousState)
 	return (
-		<ScrollView className='flex bg-red-200 pt-10'>
-			<View className='flex flex-row justify-around  bg-blue-200 mr-auto ml-auto w-10/12'>
+		<ScrollView className='flex pt-10'>
+			<View className='flex gap-4 mb-4 flex-row  bg-blue-200 mr-auto ml-auto w-10/12'>
 				<MaterialIcons
 					name='notifications-none'
 					size={27}
@@ -16,25 +24,80 @@ const Notificaciones = () => {
 				<Text className='text-center text-violeta text-2xl text-bold'>
 					Notificaciones
 				</Text>
-				<View style={styles.container}>
+			</View>
+			<View className='flex justify-center items-center bg-blue-200 w-10/12 mr-auto ml-auto'>
+				<View className='flex h-10 flex-row border-b-[1px] border-gris_medio justify-between items-center w-full'>
+					<Text className='text-xl'>Pausar todas</Text>
 					<Switch
-						trackColor={{ false: '#767577', true: '#81b0ff' }}
-						thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-						ios_backgroundColor='#3e3e3e'
-						onValueChange={toggleSwitch}
-						value={isEnabled}
+						className='flex justify-center items-center'
+						trackColor={{
+							false: colors.gris_background,
+							true: colors.gris_background,
+						}}
+						thumbColor={isAll ? colors.naranja_claro : colors.gris_medio}
+						onValueChange={toggleAll}
+						value={isAll}
+					/>
+				</View>
+			</View>
+			<View className='flex justify-center items-center bg-blue-200 w-10/12 mr-auto ml-auto'>
+				<View className='flex h-10 flex-row border-b-[1px] border-gris_medio justify-between items-center w-full'>
+					<Text className='text-xl'>Por transferencias</Text>
+					<Switch
+						className='flex justify-center items-center'
+						trackColor={{
+							false: colors.gris_background,
+							true: colors.gris_background,
+						}}
+						thumbColor={istransfer ? colors.naranja_claro : colors.gris_medio}
+						onValueChange={toggleTransfer}
+						value={istransfer}
+					/>
+				</View>
+			</View>
+			<View className='flex justify-center items-center bg-blue-200 w-10/12 mr-auto ml-auto'>
+				<View className='flex h-10 flex-row border-b-[1px] border-gris_medio justify-between items-center w-full'>
+					<Text className='text-xl'>Por ingresos</Text>
+					<Switch
+						className='flex justify-center items-center'
+						trackColor={{
+							false: colors.gris_background,
+							true: colors.gris_background,
+						}}
+						thumbColor={isIngresos ? colors.naranja_claro : colors.gris_medio}
+						onValueChange={toggleIngresos}
+						value={isIngresos}
+					/>
+				</View>
+				<View className='flex h-10 flex-row border-b-[1px] border-gris_medio justify-between items-center w-full'>
+					<Text className='text-xl'>Pagos realizados</Text>
+					<Switch
+						className='flex justify-center items-center'
+						trackColor={{
+							false: colors.gris_background,
+							true: colors.gris_background,
+						}}
+						thumbColor={isPagos ? colors.naranja_claro : colors.gris_medio}
+						onValueChange={togglePagos}
+						value={isPagos}
+					/>
+				</View>
+				<View className='flex h-10 flex-row border-b-[1px] border-gris_medio justify-between items-center w-full'>
+					<Text className='text-xl'>Links creados</Text>
+					<Switch
+						className='flex justify-center items-center'
+						trackColor={{
+							false: colors.gris_background,
+							true: colors.gris_background,
+						}}
+						thumbColor={isLinks ? colors.naranja_claro : colors.gris_medio}
+						onValueChange={toggleLinks}
+						value={isLinks}
 					/>
 				</View>
 			</View>
 		</ScrollView>
 	)
 }
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
 
 export default Notificaciones
