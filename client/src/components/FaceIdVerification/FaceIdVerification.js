fca0c65fbe0fe9bfd034a972ff1075faf094359f
DanielVP26 <daniel.villega3000@gmail.com>
2023-06-07 11:30:06 -0400

    install SecureStorage, LocalAuth dependency

--- client/src/components/FaceIdVerification/FaceIdVerification.js
+++ client/src/components/FaceIdVerification/FaceIdVerification.js
@@ -1,20 +1,64 @@
-import { View, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {
+	View,
+	Text,
+	Image,
+	TouchableOpacity,
+	ActivityIndicator,
+	Alert,
+	Pressable,
+} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
-import fondo from '../../Images/wave.webp'
+import * as LocalAuthentication from 'expo-local-authentication'
 import { useFonts } from 'expo-font'
 import { useCallback } from 'react'
-import fingerprint from '../../../assets/faceIdVioleta.png'
 import * as SplashScreen from 'expo-splash-screen'
 import { useNavigation } from '@react-navigation/native'
 import colors from '../../utils/colors'
+import fondo from '../../Images/wave.webp'
+import faceId from '../../../assets/faceIdVioleta.png'
 
 SplashScreen.preventAutoHideAsync()
+
 const FaceIdVerification = () => {
 	const navigation = useNavigation()
 	const [fontsLoaded] = useFonts({
 		'poppins-medium': require('../../../assets/poppinsFonts/Poppins-Medium.ttf'),
 		'poppins-regular': require('../../../assets/poppinsFonts/Poppins-Regular.ttf'),
 	})
+
+	const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+	useEffect(() => {
+		authenticate()
+	}, [])
+
+	const authenticate = async () => {
+		const isFaceIdSupported = await LocalAuthentication.hasHardwareAsync()
+
+		if (isFaceIdSupported) {
+			const result = await LocalAuthentication.authenticateAsync({
+				promptMessage: 'Autentique su rostro',
+				cancelLabel: 'Cancelar',
+				disableDeviceFallback: true,
+				authenticationType:
+					LocalAuthentication.AuthenticationType.FACIAL_RECOGNITION,
+			})
+
+			if (result.success) {
+				setIsAuthenticated(true)
+				navigation.navigate('Bar')
+			} else {
+				setIsAuthenticated(false)
+				Alert.alert('Error', 'No se pudo autenticar')
+				navigation.navigate('Security')
+			}
+		} else {
+			setIsAuthenticated(false)
+			Alert.alert('Error', 'Face ID no es compatible')
+			navigation.navigate('Security')
+		}
+	}
 
 	const onLayoutButtom = useCallback(async () => {
 		if (fontsLoaded) {
@@ -23,7 +67,7 @@ const FaceIdVerification = () => {
 	}, [fontsLoaded])
 
 	if (!fontsLoaded) {
-		return <ActivityIndicator size="large" color={colors.violeta} />
+		return <ActivityIndicator size='large' color={colors.violeta} />
 	}
 
 	return (
@@ -39,11 +83,7 @@ const FaceIdVerification = () => {
 					className='absolute w-full object-fill justify-center items-center -bottom-5'
 				/>
 				<View
-					style={{
-						height: '100%',
-						alignItems: 'center',
-						marginTop: '45%',
-					}}
+					style={{ height: '100%', alignItems: 'center', marginTop: '45%' }}
 				>
 					<Text
 						className='text-center text-white font-semibold text-2xl'
@@ -54,7 +94,7 @@ const FaceIdVerification = () => {
 						Verificar su identidad:
 					</Text>
 					<Text
-						className='text-center text-white  text-base mt-8 px-12'
+						className='text-center text-white text-base mt-8 px-12'
 						style={{
 							fontFamily: 'poppins-regular',
 						}}
@@ -64,10 +104,9 @@ const FaceIdVerification = () => {
 					<View>
 						<View className='flex justify-center items-center mt-12'>
 							<View className='bg-white w-28 h-28 rounded-full flex justify-center items-center'>
-								<Image
-									source={fingerprint}
-									className=' h-[55] object-scale-down'
-								/>
+								<Pressable onPress={authenticate}>
+									<Image source={faceId} className='h-[55] object-scale-down' />
+								</Pressable>
 							</View>
 						</View>
 						<Text
