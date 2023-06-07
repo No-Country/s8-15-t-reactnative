eae00b56194683885283bf873c3e0d40c545ff34
ccama22 <ccamacruz2018@gmail.com>
2023-06-07 18:57:43 -0500

    backend userEdit v3

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,7 +13,7 @@ const router = Router()
 
 router.post('/login', userLogin, userLogin)
 router.post('/register', verifiRegisterData, userRegister)
-router.put('/user-edit/:userId', userEdit);
+router.put('/user-edit/:id', userEdit);
 router.post('/sendSMS', userSms)
 router.use('/user', userRoutes );
 router.use('/category', categoryRoutes );
