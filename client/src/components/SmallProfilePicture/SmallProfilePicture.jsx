b74ee87aae16c9807a089ace2f10dd8b5e0d0382
ccama22 <ccamacruz2018@gmail.com>
2023-06-08 06:23:07 -0500

    home user v2.3

--- client/src/components/SmallProfilePicture/SmallProfilePicture.jsx
+++ client/src/components/SmallProfilePicture/SmallProfilePicture.jsx
@@ -1,8 +1,9 @@
 import { Image, TouchableOpacity } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
-
+import { useSelector } from 'react-redux'
 const SmallProfilePicture = () => {
 	const navigation = useNavigation()
+	const user = useSelector(state => state.user?.user);
 	return (
 		<TouchableOpacity
 			onPress={() => navigation.navigate('Profile')}
@@ -11,7 +12,7 @@ const SmallProfilePicture = () => {
 			<Image
 				className='h-[40px] w-[40px] rounded-full'
 				source={{
-					uri: 'https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg',
+					uri: user.picture ? user.picture : 'https://d500.epimg.net/cincodias/imagenes/2016/07/04/lifestyle/1467646262_522853_1467646344_noticia_normal.jpg',
 				}}
 			/>
 		</TouchableOpacity>
